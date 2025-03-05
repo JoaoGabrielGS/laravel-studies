008cb0b49a13e07ff2dd2b28a661ba21543b59f3
Biel <bielgolin@gmail.com>
2025-03-05 18:18:58 -0300

    feat(series): adicionado métodos de remoção e edição de séries

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -9,8 +9,9 @@ class SeriesController extends Controller
 {
     public function index(Request $request) {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
-        return view('series.index', compact('series'));
+        return view('series.index', compact('series'))->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create() {
@@ -18,11 +19,24 @@ class SeriesController extends Controller
     }
 
     public function store(Request $request) {
-        $nomeSerie = $request->input('nome');
-        $serie = new Serie();
-        $serie->nome = $nomeSerie;
-        $serie->save();
+        $serie = Serie::create($request->all());
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso!");
+    }
 
-        return redirect('/series');
+    public function destroy(Serie $series) {
+        $series->delete();
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso!");
+    }
+
+    public function edit(Serie $series) {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request) {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso!");
     }
 }
